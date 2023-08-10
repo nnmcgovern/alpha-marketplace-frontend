27f2631dbf04c280ef410239874efefe57619d64
Natalie Nicole <nataliemcgovern17@gmail.com>
2023-08-09 20:28:18 -0400

    Navbar.jsx: add navlinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,14 @@
 import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <NavLink to="/">
+        <img src="" alt="alpha marketplace logo" />
+      </NavLink>{" "}
+      <NavLink to="/all-cars">Shop for Cars</NavLink>
+      <NavLink to="/login">Login</NavLink>
+      <NavLink to="/cart">Cart</NavLink>
+    </div>
+  );
 }

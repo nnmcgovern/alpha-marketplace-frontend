1234a432113088b34a2eac773afa76e82ef88770
Keith Lee <khlee930121@gmail.com>
2023-08-10 00:45:29 -0400

    create sidebar/event handler/make

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,22 @@
 import React, { useState } from 'react';
 
 export default function Sidebar() {
-  return <div>Sidebar</div>;
+  const [activeMenu, setActiveMenu] = useState(null); //update the status of active menu
+
+  const handleMenuClick = (menu) => {
+    setActiveMenu(activeMenu === menu ? null : menu);
+  }; //event handle function to drop down the menu or not
+
+  return (
+    <div>
+      <div onClick={() => handleMenuClick('make')}>Make</div>
+      {activeMenu === 'make' && (
+        <div>
+          <div>Option 1</div>
+          <div>Option 2</div>
+        </div>
+      )}
+      {/*short-circuit evaluation; if both true, show the menu */}
+    </div>
+  );
 }

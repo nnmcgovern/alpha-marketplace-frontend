f1de47503742937bdf9706458067bec51cde2b79
Natalie Nicole <nataliemcgovern17@gmail.com>
2023-08-10 09:40:08 -0400

    CarDetailModal structure

--- src/components/CarDetailModal.jsx
+++ src/components/CarDetailModal.jsx
@@ -1,3 +1,18 @@
-export default function CarDetailModal() {
-  return <div>CarDetailModal</div>;
+export default function CarDetailModal({ car, setShowCarDetailModal }) {
+  const handleClick = (e) => {
+    // add to cart
+  };
+
+  return (
+    <div>
+      <img src={car.image} alt={`${car.make} ${car.model}`} />
+      <p>{car.make}</p>
+      <p>{car.model}</p>
+      <p>{car.type}</p>
+      <p>{car.year}</p>
+      <p>{car.color}</p>
+      <p>${car.price}</p>
+      <button onClick={handleClick}>Add to Cart</button>
+    </div>
+  );
 }

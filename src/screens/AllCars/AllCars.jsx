7ac925039acdcf537a5c956247975dc09733232f
Keith Lee <khlee930121@gmail.com>
2023-08-11 11:46:25 -0400

    create my_accout list items files

--- src/screens/AllCars/AllCars.jsx
+++ src/screens/AllCars/AllCars.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from "react";
-import Sidebar from "../../components/Sidebar/Sidebar";
-import CarPreview from "../../components/CarPreview/CarPreview";
-import { getCars } from "../../services/cars";
-import "./AllCars.css";
+import { useState, useEffect } from 'react';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import CarPreview from '../../components/CarPreview/CarPreview';
+import { getCars } from '../../services/cars';
+import './AllCars.css';
 
 export default function AllCars() {
   const [cars, setCars] = useState([]);
@@ -16,13 +16,14 @@ export default function AllCars() {
     // setCars(cars)
     const cars = await getCars();
     setCars(cars);
+    console.log(cars);
   }
 
   return (
     <div>
       <h1>All Cars</h1>
       <Sidebar />
-      <div className="allcars-grid">
+      <div className='allcars-grid'>
         {cars.map((car) => (
           <CarPreview car={car} key={car._id} /> // create list of image + make, model for each car
         ))}

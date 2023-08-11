68c6df912859523a7cf9779c4702edc4c1bc0f64
Rebekah <rebekahmenn@gmail.com>
2023-08-11 14:14:19 -0400

    added users.js services

---
+++ src/services/users.js
@@ -0,0 +1,42 @@
+import jwtDecode from "jwt-decode";
+import api from "./apiConfig";
+
+export const login = async (credentials) => {
+  try {
+    const res = await api.post("/login", credentials);
+    localStorage.setItem("token", res.data.token);
+    const user = jwtDecode(res.data.token);
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const signUp = async (credentials) => {
+  try {
+    const res = await api.post("/signup", credentials);
+    localStorage.setItem("token", res.data.token);
+    const user = jwtDecode(res.data.token);
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const verify = async () => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    const res = await api.get("/verify");
+    return res.data;
+  }
+  return false;
+};
+
+export const logout = () => {
+  try {
+    localStorage.removeItem("token");
+    return true;
+  } catch (error) {
+    throw error;
+  }
+};

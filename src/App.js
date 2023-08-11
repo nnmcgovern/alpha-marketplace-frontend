7a9556520307b1d213750bc340a3509e21fcc679
Natalie Nicole <nataliemcgovern17@gmail.com>
2023-08-11 14:50:15 -0400

    add button to switch to login on create account modal

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import { Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
-import Navbar from './components/Navbar/Navbar';
-import AllCars from './screens/AllCars/AllCars';
-import Cart from './screens/Cart/Cart';
-import Home from './screens/Home/Home.jsx';
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import AllCars from "./screens/AllCars/AllCars";
+import Cart from "./screens/Cart/Cart";
+import Home from "./screens/Home/Home.jsx";
 // import Login from "./screens/Login";
-import MyAccount from './screens/MyAccount/MyAccount';
-import LoginModal from './components/LoginModal/LoginModal';
-import CreateAccountModal from './components/CreateAccountModal/CreateAccountModal';
-import './App.css';
+import MyAccount from "./screens/MyAccount/MyAccount";
+import LoginModal from "./components/LoginModal/LoginModal";
+import CreateAccountModal from "./components/CreateAccountModal/CreateAccountModal";
+import "./App.css";
 
 function App() {
   const [showLoginModal, setShowLoginModal] = useState(false);
   const [showCreateAccountModal, setShowCreateAccountModal] = useState(false);
 
   return (
-    <div className='App'>
+    <div className="App">
       <Navbar setShowLoginModal={setShowLoginModal} />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/all-cars' element={<AllCars />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='myaccount' element={<MyAccount />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/all-cars" element={<AllCars />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="myaccount" element={<MyAccount />} />
         {/* <Route path="/login" element={<Login />} /> */}
       </Routes>
       {showLoginModal && (
@@ -33,6 +33,7 @@ function App() {
       {showCreateAccountModal && (
         <CreateAccountModal
           setShowCreateAccountModal={setShowCreateAccountModal}
+          setShowLoginModal={setShowLoginModal}
         />
       )}
     </div>

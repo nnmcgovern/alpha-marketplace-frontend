9f16362c4a1a04dfeff0f1f654555a5de5cf4074
Rebekah <rebekahmenn@gmail.com>
2023-08-11 13:52:57 -0400

    updated apiConfig with getToken

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
+const getToken = () => {
+  return new Promise((resolve) => {
+    resolve(`Bearer ${localStorage.getItem("token") || null}`);
+  });
+};
+
 const apiURLs = {
   development: "http://localhost:3000/api",
+  production:
+    "https://alpha-marketplace-backend-18469668e160.herokuapp.com/api",
 };
 
 let baseURL = "";
@@ -9,11 +17,22 @@ let baseURL = "";
 if (window.location.hostname === "localhost") {
   baseURL = apiURLs.development;
 } else {
-  // for later
+  baseURL = apiURLs.production;
 }
 
 const api = axios.create({
   baseURL,
 });
+
+api.interceptors.request.use(
+  async (config) => {
+    config.headers["Authorization"] = await getToken();
+    return config;
+  },
+  function (error) {
+    console.log("Request error: ", error);
+    return Promise.reject(error);
+  }
+);
 
 export default api;

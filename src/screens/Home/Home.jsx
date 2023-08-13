d064203fb869f58e166d7265c9fe06c531fc7347
Rebekah <rebekahmenn@gmail.com>
2023-08-13 16:08:39 -0400

    home screen hero image

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,7 +1,9 @@
+import "./Home.css"
+
 export default function Home() {
   return (
-    <div>
-      <h1>Is this HOME?!!?!</h1>
+    <div className="home">
+      <h1 className="home-text">Welcome to Alpha Market your go-to platform for buying and selling cars online</h1>
     </div>
   );
 }

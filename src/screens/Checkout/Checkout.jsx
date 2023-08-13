2ee52515e85d5f34ee388cc3483ae6c53c95b482
Rebekah <rebekahmenn@gmail.com>
2023-08-13 19:38:50 -0400

    added changes to Checkout.jsx

--- src/screens/Checkout/Checkout.jsx
+++ src/screens/Checkout/Checkout.jsx
@@ -7,6 +7,7 @@ import api from "../../services/apiConfig";
 export default function Checkout() {
   const [cars, setCars] = useState(JSON.parse(localStorage.getItem("cart")));
   const [rerender, setRerender] = useState(false);
+  const navigate = useNavigate();
   const [userData, setUserData] = useState({
     name: "",
     address: "",
@@ -18,7 +19,7 @@ export default function Checkout() {
     setCars(JSON.parse(localStorage.getItem("cart")));
     
     // Calculate total
-    const totalPrice = cars?.reduce((acc, car) => acc + car.price, 0); || 0;
+    const totalPrice = cars?.reduce((acc, car) => acc + car.price, 0) || 0;
     setTotal(totalPrice);
   }, [rerender]);
 
@@ -31,31 +32,19 @@ export default function Checkout() {
 
     const handleSubmit = async (e) => {
       e.preventDefault();
-      // Send data to backend
-
       const carIds = cars.map((car) => car._id);
 
       try {
-        const res = await fetch(`${process.env.REACT_APP_API_URL}/cars/checkout`, {
-          method: "POST",
-          body: JSON.stringify({ carIds, ...userData }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-        );
-        const data = await res.json();
+        const res = await api.post(`/cars/checkout`, { carIds, ...userData});
 
-        if (data.success) {
-          // Success alert
+        if (res.data.success) {
           alert("Thank you for your purchase!");
           // Clear cart
           localStorage.setItem("cart", JSON.stringify([]));
           setRerender(!rerender);
-
           // redirect to homepage
+          navigate("/");
         } else {
-          // Error alert
           alert("Something went wrong. Please try again.");
         }
       } catch (error) {
@@ -64,20 +53,52 @@ export default function Checkout() {
     };
         
 
-    
-
     return (
       <div>
         <h1>Checkout</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div className="user-details">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={userData.name}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            placeholder="Address fake"
+            name="address"
+            value={userData.address}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            placeholder="Credit Card fake"
+            name="creditcard"
+            value={userData.creditcard}
+            onChange={handleInputChange}
+          />
+        </div>
+
         <div className="checkout-item-list-container">
           {cars &&
             cars.map((car) => (
               <CheckoutItem car={car} setRerender={setRerender} key={car._id} />
             ))}
         </div>
+
+        <div className="total-price">
+          <h3>Total: ${total}</h3>
+        </div>
+        
+        <button type="submit">Complete Checkout</button>
+      </form>
       </div>
     );
-  }
+}
+  
             
           
   

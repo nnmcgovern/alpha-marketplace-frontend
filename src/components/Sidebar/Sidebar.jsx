a2fd77112b8156413a407e6ee4060b7fd4d90feb
Keith Lee <khlee930121@gmail.com>
2023-08-14 14:41:05 -0400

    sidebar css + filter for 'make'

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react';
-
-export default function Sidebar() {
+import { checked, setChecked } from '../../screens/AllCars/AllCars.jsx';
+export default function Sidebar({ checked, setChecked }) {
   const [activeMenu, setActiveMenu] = useState(false); //update the status of active menu
-  const [checked, setChecked] = useState({
-    box1: false,
-    box2: false,
-  });
+
   const [activeModel, setActiveModel] = useState(false);
 
   const handleMenuClick = (menu) => {
@@ -22,29 +19,160 @@ export default function Sidebar() {
       ...prev,
       [name]: checked,
     }));
+    //if (all boxes unchecked) { setChecked({all:true}) return }
   };
+  //if none of the boxes are checked, all true, then return
+  //if one or more of the boxes are chekcked, make all false. and make appropriate boxes true
+  //setChecked(...prev., [name]: checked,)
 
+  //in all cars, filter through... all cars and return just the cars that are checked. then map through that
+  //returned filter and render those on the page
+  //
   return (
-    <div>
+    <div className='sidebar'>
       <div onClick={() => handleMenuClick('make')}>Make</div>
       {activeMenu && (
         <div>
-          <input
-            type='checkbox'
-            id='BMW'
-            name='bmw'
-            value={checked.box1}
-            onChange={handleCheckedChange}
-          />
-          <label htmlFor='BMW'>BMW</label>
-          <input
-            type='checkbox'
-            id='Nissan'
-            name='nissan'
-            value={checked.box2}
-            onChange={handleCheckedChange}
-          />
-          <label htmlFor='Nissan'>Nissan</label>
+          <div className='checkbox-group'>
+            <input
+              type='checkbox'
+              id='Audi'
+              name='audi'
+              value={checked.box1}
+              onChange={handleCheckedChange}
+            />
+            <label htmlFor='Audi'>Audi</label>
+          </div>
+          <div className='checkbox-group'>
+            <input
+              type='checkbox'
+              id='BMW'
+              name='bmw'
+              value={checked.box2}
+              onChange={handleCheckedChange}
+            />
+            <label htmlFor='BMW'>BMW</label>
+          </div>
+          <div className='checkbox-group'>
+            <input
+              type='checkbox'
+              id='Chevrolet'
+              name='chevrolet'
+              value={checked.box3}
+              onChange={handleCheckedChange}
+            />
+            <label htmlFor='Chevrolet'>Chevrolet</label>
+          </div>
+          <div className='checkbox-group'>
+            <input
+              type='checkbox'
+              id='Dodge'
+              name='dodge'
+              value={checked.box4}
+              onChange={handleCheckedChange}
+            />
+            <label htmlFor='Dodge'>Dodge</label>
+          </div>
+          <div className='checkbox-group'>
+            <input
+              type='checkbox'
+              id='Ford'
+              name='ford'
+              value={checked.box5}
+              onChange={handleCheckedChange}
+            />
+            <label htmlFor='Ford'>Ford</label>
+          </div>
+          <div className='checkbox-group'>
+            <input
+              type='checkbox'
+              id='Honda'
+              name='honda'
+              value={checked.box6}
+              onChange={handleCheckedChange}
+            />
+            <label htmlFor='Honda'>Honda</label>
+          </div>
+          <div className='checkbox-group'>
+            <input
+              type='checkbox'
+              id='Hyundai'
+              name='hyundai'
+              value={checked.box7}
+              onChange={handleCheckedChange}
+            />
+            <label htmlFor='Hyundai'>Hyundai</label>
+          </div>
+          <div className='checkbox-group'>
+            <input
+              type='checkbox'
+              id='Jeep'
+              name='jeep'
+              value={checked.box8}
+              onChange={handleCheckedChange}
+            />
+            <label htmlFor='Jeep'>Jeep</label>
+          </div>
+          <div className='checkbox-group'>
+            <input
+              type='checkbox'
+              id='Kia'
+              name='kia'
+              value={checked.box9}
+              onChange={handleCheckedChange}
+            />
+            <label htmlFor='Kia'>Kia</label>
+          </div>
+          <div className='checkbox-group'>
+            <input
+              type='checkbox'
+              id='Mazda'
+              name='mazda'
+              value={checked.box10}
+              onChange={handleCheckedChange}
+            />
+            <label htmlFor='Mazda'>Mazda</label>
+          </div>
+          <div className='checkbox-group'>
+            <input
+              type='checkbox'
+              id='Mercedes-Benz'
+              name='mercedes-benz'
+              value={checked.box11}
+              onChange={handleCheckedChange}
+            />
+            <label htmlFor='Mercedes-Benz'>Mercedes-Benz</label>
+          </div>
+          <div className='checkbox-group'>
+            <input
+              type='checkbox'
+              id='Nissan'
+              name='nissan'
+              value={checked.box12}
+              onChange={handleCheckedChange}
+            />
+            <label htmlFor='Nissan'>Nissan</label>
+          </div>
+          <div className='checkbox-group'>
+            <input
+              type='checkbox'
+              id='Subaru'
+              name='subaru'
+              value={checked.box13}
+              onChange={handleCheckedChange}
+            />
+            <label htmlFor='Subaru'>Subaru</label>
+          </div>
+          <div className='checkbox-group'>
+            <input
+              type='checkbox'
+              id='Volkswagen'
+              name='volkswagen'
+              value={checked.box14}
+              onChange={handleCheckedChange}
+            />
+            <label htmlFor='Volkswagen'>Volkswagen</label>
+          </div>
         </div>
       )}
       {/*short-circuit evaluation; if both true, show the menu */}
--- src/screens/AllCars/AllCars.jsx
+++ src/screens/AllCars/AllCars.jsx
@@ -1,11 +1,13 @@
-import { useState, useEffect } from "react";
-import Sidebar from "../../components/Sidebar/Sidebar";
-import CarPreview from "../../components/CarPreview/CarPreview";
-import { getCars } from "../../services/cars";
-import "./AllCars.css";
+import { useState, useEffect } from 'react';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import CarPreview from '../../components/CarPreview/CarPreview';
+import { getCars } from '../../services/cars';
+import './AllCars.css';
+import '../../components/Sidebar/Sidebar.css';
 
 export default function AllCars() {
   const [cars, setCars] = useState([]);
+  const [checked, setChecked] = useState({});
 
   useEffect(() => {
     fetchCars();
@@ -16,14 +18,28 @@ export default function AllCars() {
     setCars(cars);
   }
 
+  let arr = [];
+  for (const make in checked) {
+    if (checked[make]) arr.push(make.toLowerCase());
+  }
+
   return (
     <div>
-      <Sidebar />
-      <div className="all-cars-grid">
-        {cars.map((car) => (
-          <CarPreview car={car} key={car._id} /> // create list of image + make, model for each car
-        ))}
+      <Sidebar checked={checked} setChecked={setChecked} />
+      <div className='all-cars-grid'>
+        {!Object.values(checked).includes(true) &&
+          cars.map((car) => (
+            <CarPreview car={car} key={car._id} /> // create list of image + make, model for each car
+          ))}
+        {cars
+          .filter((car) => arr.includes(car.make.toLowerCase()))
+          .map((car) => (
+            <CarPreview car={car} key={car._id} />
+          ))}
       </div>
     </div>
   );
 }
+//if (checked.all === true) {
+//{cars.map(car => (CarPreview... />))}}
+//else {cars.filter(car.make === )}

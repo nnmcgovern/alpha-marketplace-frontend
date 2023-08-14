41fe57d888c449e19929c55f9a0d8e0b6d747e0b
Natalie Nicole <nataliemcgovern17@gmail.com>
2023-08-14 09:41:26 -0400

    my account in progress

--- src/screens/MyAccount/MyAccount.jsx
+++ src/screens/MyAccount/MyAccount.jsx
@@ -1,37 +1,47 @@
-import { useState } from 'react';
-import NewCarModal from '../../components/NewCarModal/NewCarModal';
-import MyItems from '../MyItems/MyItems';
+import { useState } from "react";
+import NewCarModal from "../../components/NewCarModal/NewCarModal";
+import MyItem from "../../components/MyItem/MyItem";
 
 export default function MyAccount() {
   const [showNewCarModal, setShowNewCarModal] = useState(false);
-  const [showItemsModal, setShowItemsModal] = useState(true);
-  const items = [
-    // example car
-    {
-      image: '',
-      maker: 'Lamborghini',
-      model: 'Urus',
-      type: 'SUV',
-      year: 2023,
-      color: 'Yellow',
-      price: 500000,
-    },
-    {
-      image: '',
-      maker: 'Porsche',
-      model: '911 TurboS',
-      type: 'Coupe',
-      year: 2023,
-      color: 'Red',
-      price: 300000,
-    },
-  ];
+  const [myItems, setMyItems] = useState([]);
+  // const items = [
+  //   // example car
+  //   {
+  //     image: '',
+  //     maker: 'Lamborghini',
+  //     model: 'Urus',
+  //     type: 'SUV',
+  //     year: 2023,
+  //     color: 'Yellow',
+  //     price: 500000,
+  //   },
+  //   {
+  //     image: '',
+  //     maker: 'Porsche',
+  //     model: '911 TurboS',
+  //     type: 'Coupe',
+  //     year: 2023,
+  //     color: 'Red',
+  //     price: 300000,
+  //   },
+  // ];
   return (
     <div>
-      <h1>My Account</h1>
-      {showItemsModal && <MyItems items={items} />}
+      <div className="my-acct-sidebar">
+        <button>My Listings</button>
+        <button>My Favorites</button>
+      </div>
+      <div className="my-acct-listings-header">
+        <h1>My Account</h1>
+        <button>Add New</button>
+      </div>
+      {myItems.map((item) => (
+        <MyItem car={item} />
+      ))}
+      {/* {items && <MyItems items={items} />} */}
       {showNewCarModal && (
-        <NewCarModal setShowNewCarModal={setShowNewCarModal} />
+        <NewCarModal setShowNewCarModal={setShowNewCarModal} /> // should appear when 'add new' button is clicked
       )}
     </div>
   );

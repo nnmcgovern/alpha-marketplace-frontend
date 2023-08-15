5acbf115a50bc691b3775eb72239420cb5a2a23b
Keith Lee <khlee930121@gmail.com>
2023-08-15 14:37:22 -0400

    navbar media update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-import { useEffect } from "react";
-import { NavLink, useNavigate } from "react-router-dom";
-import "./Navbar.css";
+import { useEffect } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import './Navbar.css';
 
 export default function Navbar({ setShowLoginModal, user, setUser }) {
   const navigate = useNavigate();
@@ -10,57 +10,57 @@ export default function Navbar({ setShowLoginModal, user, setUser }) {
   };
 
   const handleClickLogout = (e) => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("user");
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
     setUser(null);
-    navigate("/");
+    navigate('/');
   };
 
   useEffect(() => {}, [user]);
 
   return (
-    <div className="navbar">
-      <div className="navbar_logo">
-        <NavLink id="link" to="/">
+    <div className='navbar'>
+      <div className='navbar_logo'>
+        <NavLink id='link' to='/'>
           {/* <img src='' alt='alpha marketplace logo' /> */}
-          <i className="fa-solid fa-car"></i>
-          <p className="logo_font">Alpha Marketplace</p>
-        </NavLink>{" "}
+          <i className='fa-solid fa-car'></i>
+          <p className='logo_font'>Alpha Marketplace</p>
+        </NavLink>{' '}
       </div>
-      <ul className="navbar_menu">
+      <ul className='navbar_menu'>
         <li>
-          <NavLink id="link" to="/">
+          <NavLink id='link' to='/'>
             HOME
           </NavLink>
         </li>
         <li>
-          <NavLink id="link" to="/all-cars">
+          <NavLink id='link' to='/all-cars'>
             SHOP
           </NavLink>
         </li>
         {/* <NavLink id='link' to="/login">Login</NavLink> */}
         <li>
-          <NavLink id="link" to="/cart">
+          <NavLink id='link' to='/cart'>
             CART
           </NavLink>
         </li>
         {!user && (
           <li>
-            <div id="link" onClick={handleClickLogin}>
+            <div id='link' onClick={handleClickLogin}>
               LOGIN
             </div>
           </li>
         )}
         {user && (
           <li>
-            <NavLink id="link" to="/myaccount">
+            <NavLink id='link' to='/myaccount'>
               MY ACCOUNT
             </NavLink>
           </li>
         )}
         {user && (
           <li>
-            <div id="link" onClick={handleClickLogout}>
+            <div id='link' onClick={handleClickLogout}>
               LOGOUT
             </div>
           </li>

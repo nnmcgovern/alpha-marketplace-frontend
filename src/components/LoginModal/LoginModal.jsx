7a6e43c5e5a984185c378b9b174cca45b11a6566
Natalie Nicole <nataliemcgovern17@gmail.com>
2023-08-16 10:13:12 -0400

    if not logged in, user is redirected back to cart + login modal appears on clicking checkout button

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import { login } from "../../services/users.js";
+import { useNavigate } from "react-router-dom";
 import "./LoginModal.css";
 
 export default function LoginModal({
   setShowLoginModal,
   setShowCreateAccountModal,
   setUser,
+  redirectOnCancel,
 }) {
   const [form, setForm] = useState({
     username: "",
@@ -14,6 +16,7 @@ export default function LoginModal({
     errorMsg: "",
   });
   const [status, setStatus] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     document.body.classList.add("modal-open"); // add css to disable body scrolling
@@ -52,6 +55,9 @@ export default function LoginModal({
   };
 
   const handleClickCancel = (e) => {
+    if (redirectOnCancel) {
+      navigate("/cart");
+    }
     setShowLoginModal(false);
     document.body.classList.remove("modal-open");
   };

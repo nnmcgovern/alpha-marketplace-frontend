f30586957f18784960f2a87d917d3b185e003c96
Natalie Nicole <nataliemcgovern17@gmail.com>
2023-08-17 09:10:00 -0400

    remove unused import from Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,4 @@
-import { useState } from 'react';
-import {
-  checked,
-  setChecked,
-  setSelectedMake,
-  selectedMake,
-} from '../../screens/AllCars/AllCars.jsx';
+import { useState } from "react";
 
 export default function Sidebar({
   checked,
@@ -23,7 +17,7 @@ export default function Sidebar({
     if (checked) {
       setSelectedMake(name);
     } else if (selectedMake === name) {
-      setSelectedMake('');
+      setSelectedMake("");
     }
     setChecked((prev) => ({
       ...prev,
@@ -32,149 +26,149 @@ export default function Sidebar({
   };
 
   return (
-    <div className='sidebar'>
-      <div onClick={() => handleMenuClick('make')}>Make</div>
+    <div className="sidebar">
+      <div onClick={() => handleMenuClick("make")}>Make</div>
       {activeMenu && (
         <div>
-          <div className='checkbox-group'>
+          <div className="checkbox-group">
             <input
-              type='checkbox'
-              id='Audi'
-              name='audi'
+              type="checkbox"
+              id="Audi"
+              name="audi"
               value={checked.audi}
               onChange={handleCheckedChange}
             />
-            <label htmlFor='Audi'>Audi</label>
+            <label htmlFor="Audi">Audi</label>
           </div>
-          <div className='checkbox-group'>
+          <div className="checkbox-group">
             <input
-              type='checkbox'
-              id='BMW'
-              name='bmw'
+              type="checkbox"
+              id="BMW"
+              name="bmw"
               value={checked.bmw}
               onChange={handleCheckedChange}
             />
-            <label htmlFor='BMW'>BMW</label>
+            <label htmlFor="BMW">BMW</label>
           </div>
-          <div className='checkbox-group'>
+          <div className="checkbox-group">
             <input
-              type='checkbox'
-              id='Chevrolet'
-              name='chevrolet'
+              type="checkbox"
+              id="Chevrolet"
+              name="chevrolet"
               value={checked.box3}
               onChange={handleCheckedChange}
             />
-            <label htmlFor='Chevrolet'>Chevrolet</label>
+            <label htmlFor="Chevrolet">Chevrolet</label>
           </div>
-          <div className='checkbox-group'>
+          <div className="checkbox-group">
             <input
-              type='checkbox'
-              id='Dodge'
-              name='dodge'
+              type="checkbox"
+              id="Dodge"
+              name="dodge"
               value={checked.box4}
               onChange={handleCheckedChange}
             />
-            <label htmlFor='Dodge'>Dodge</label>
+            <label htmlFor="Dodge">Dodge</label>
           </div>
-          <div className='checkbox-group'>
+          <div className="checkbox-group">
             <input
-              type='checkbox'
-              id='Ford'
-              name='ford'
+              type="checkbox"
+              id="Ford"
+              name="ford"
               value={checked.box5}
               onChange={handleCheckedChange}
             />
-            <label htmlFor='Ford'>Ford</label>
+            <label htmlFor="Ford">Ford</label>
           </div>
-          <div className='checkbox-group'>
+          <div className="checkbox-group">
             <input
-              type='checkbox'
-              id='Honda'
-              name='honda'
+              type="checkbox"
+              id="Honda"
+              name="honda"
               value={checked.box6}
               onChange={handleCheckedChange}
             />
-            <label htmlFor='Honda'>Honda</label>
+            <label htmlFor="Honda">Honda</label>
           </div>
-          <div className='checkbox-group'>
+          <div className="checkbox-group">
             <input
-              type='checkbox'
-              id='Hyundai'
-              name='hyundai'
+              type="checkbox"
+              id="Hyundai"
+              name="hyundai"
               value={checked.box7}
               onChange={handleCheckedChange}
             />
-            <label htmlFor='Hyundai'>Hyundai</label>
+            <label htmlFor="Hyundai">Hyundai</label>
           </div>
-          <div className='checkbox-group'>
+          <div className="checkbox-group">
             <input
-              type='checkbox'
-              id='Jeep'
-              name='jeep'
+              type="checkbox"
+              id="Jeep"
+              name="jeep"
               value={checked.box8}
               onChange={handleCheckedChange}
             />
-            <label htmlFor='Jeep'>Jeep</label>
+            <label htmlFor="Jeep">Jeep</label>
           </div>
-          <div className='checkbox-group'>
+          <div className="checkbox-group">
             <input
-              type='checkbox'
-              id='Kia'
-              name='kia'
+              type="checkbox"
+              id="Kia"
+              name="kia"
               value={checked.box9}
               onChange={handleCheckedChange}
             />
-            <label htmlFor='Kia'>Kia</label>
+            <label htmlFor="Kia">Kia</label>
           </div>
-          <div className='checkbox-group'>
+          <div className="checkbox-group">
             <input
-              type='checkbox'
-              id='Mazda'
-              name='mazda'
+              type="checkbox"
+              id="Mazda"
+              name="mazda"
               value={checked.box10}
               onChange={handleCheckedChange}
             />
-            <label htmlFor='Mazda'>Mazda</label>
+            <label htmlFor="Mazda">Mazda</label>
           </div>
-          <div className='checkbox-group'>
+          <div className="checkbox-group">
             <input
-              type='checkbox'
-              id='Mercedes-Benz'
-              name='mercedes-benz'
+              type="checkbox"
+              id="Mercedes-Benz"
+              name="mercedes-benz"
               value={checked.box11}
               onChange={handleCheckedChange}
             />
-            <label htmlFor='Mercedes-Benz'>Mercedes-Benz</label>
+            <label htmlFor="Mercedes-Benz">Mercedes-Benz</label>
           </div>
-          <div className='checkbox-group'>
+          <div className="checkbox-group">
             <input
-              type='checkbox'
-              id='Nissan'
-              name='nissan'
+              type="checkbox"
+              id="Nissan"
+              name="nissan"
               value={checked.box12}
               onChange={handleCheckedChange}
             />
-            <label htmlFor='Nissan'>Nissan</label>
+            <label htmlFor="Nissan">Nissan</label>
           </div>
-          <div className='checkbox-group'>
+          <div className="checkbox-group">
             <input
-              type='checkbox'
-              id='Subaru'
-              name='subaru'
+              type="checkbox"
+              id="Subaru"
+              name="subaru"
               value={checked.box13}
               onChange={handleCheckedChange}
             />
-            <label htmlFor='Subaru'>Subaru</label>
+            <label htmlFor="Subaru">Subaru</label>
           </div>
-          <div className='checkbox-group'>
+          <div className="checkbox-group">
             <input
-              type='checkbox'
-              id='Volkswagen'
-              name='volkswagen'
+              type="checkbox"
+              id="Volkswagen"
+              name="volkswagen"
               value={checked.box14}
               onChange={handleCheckedChange}
             />
-            <label htmlFor='Volkswagen'>Volkswagen</label>
+            <label htmlFor="Volkswagen">Volkswagen</label>
           </div>
         </div>
       )}
